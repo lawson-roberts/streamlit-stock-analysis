20ca0cded506e1d8b70c47cb2c81f1aeb0fe1c82
Lawson Roberts <roberts.lawson@yahoo.com>
2021-09-12 23:46:50 -0500

    troubleshooting vader sentiment issue

--- app.py
+++ app.py
@@ -1,6 +1,29 @@
+import pandas as pd
+import numpy as np
+from pandas import json_normalize
+#from selenium.webdriver.support.expected_conditions import element_selection_state_to_be
+#import matplotlib.pyplot as plt
+#import seaborn as sns
 import streamlit as st
 import base64
+import plotly.express as px
 from PIL import Image
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+#import matplotlib.pyplot as plt
+import io
+from math import floor
+
+## sentiment analysis
+import re
+import nltk
+nltk.download('vader_lexicon')
+from nltk.corpus import stopwords
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+
+##stock packages
+import yfinance as yfin
+import finnhub
 
 ## web crawling packages
 import time
