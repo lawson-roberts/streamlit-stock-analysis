913c6fa1f3fa30396e67bd73446a8e1ae49f4507
lawson-roberts <81063312+lawson-roberts@users.noreply.github.com>
2025-05-04 11:28:38 -0500

    Update crypto.py
    secret upd

--- crypto.py
+++ crypto.py
@@ -1,3 +1,4 @@
+import os
 import pandas as pd
 import numpy as np
 from pandas import json_normalize
@@ -67,7 +68,11 @@ def app():
     unixtime_today = time.mktime(today.timetuple())
     unixtime_year = time.mktime(year_ago.timetuple())
     
-    finnhub_client = finnhub.Client(api_key="c3qcjnqad3i9vt5tl68g")
+    # Setup client
+    api_key = os.getenv("FINNHUB_API_KEY")
+    if not api_key:
+        raise ValueError("FINNHUB_API_KEY environment variable is not set.")
+    finnhub_client = finnhub.Client(api_key=api_key)
     symbol_df = pd.read_csv('data/crypto_symbol_df.csv')
     symbol_df['currency'] = symbol_df['description'].str[-3:]
     symbol_df_short = symbol_df[symbol_df['currency'] == 'USD']
